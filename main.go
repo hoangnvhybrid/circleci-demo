54cdc329706908e71a9a2d07a504b4c08f189416
hoangnvhybrid <hoangnv@hybrid-technologies.vn>
2020-12-08 13:21:01 +0700

    use gin framework

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	)
 
 func main() {
 	fmt.Println("hello world")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello,World!")
+	})
+	r.Run(":8086")
+
 }
