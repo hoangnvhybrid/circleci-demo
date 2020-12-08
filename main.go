54e11dccf531c602c4f99e364d65feffa63e4ac9
hoangnvhybrid <hoangnv@hybrid-technologies.vn>
2020-12-08 16:16:39 +0700

    add /index

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	)
+	"net/http"
+)
 
 func main() {
 	fmt.Println("hello world")
@@ -14,6 +15,14 @@ func main() {
 
 	r.GET("/welcome", func(c *gin.Context) {
 		c.String(200, "Page welcome!!!")
+	})
+
+	r.LoadHTMLGlob("templates/*")
+	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+	r.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main website",
+		})
 	})
 
 	r.Run(":8086")
